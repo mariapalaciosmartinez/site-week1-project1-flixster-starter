d77797b9e5b467602adc7513d600ba80e32215af
Maria Palacios Martinez <mpalaciosmartinez@mpalaci-ltmbbam.internal.salesforce.com>
2023-06-07 16:38:34 -0700

    add section

--- script.js
+++ script.js
@@ -98,6 +98,20 @@ function generateCards(movieObject) {
     let image = document.createElement('img');
     image.src= "https://image.tmdb.org/t/p/w342" + movieObject.poster_path
     document.body.insertBefore(image, averageContainer)
+
+    //create name
+    let name = document.createElement('div')
+    name.classList.add('name')
+    name.innerText = movieObject.original_title
+    document.body.insertBefore(name, averageContainer.nextSiling);
+
+    //create movie section
+    let movie = document.createElement('section');
+    movie.classList.add('name')
+    movie.appendChild(image)
+    movie.appendChild(averageContainer)
+    movie.appendChild(name)
+    document.body.appendChild(movie)
 }
 
 generateCards(firstMovie);
